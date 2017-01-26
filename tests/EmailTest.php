72bfa8195065fda190eca3149390e554af15d0ac
marineco <marinecolonge@gmail.com>
2017-01-26 10:33:54 +0100

    premier test phpunit check

---
+++ tests/EmailTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class EmailTest extends TestCase
+{
+	public function testCanBeCreatedFromValidEmailAddress() {
+		$this->assertInstanceOf(
+				Email::class,
+				Email::fromString('user@example.com')
+			);
+	}
+}
